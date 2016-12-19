3a609389adabb3f68596b92b71bc1c20692dc722
Luke Storry <luke@lukestorry.co.uk>
2016-12-19 20:40:17 +0000

    added sqrt to optimise for large primes

--- 003.py
+++ 003.py
@@ -2,13 +2,15 @@
 #https://projecteuler.net/problem=2
 #What is the largest prime factor of the number 600851475143?
 
+from math import sqrt
 def largest_prime_factor(n):
-    for i in range (2, n):
+    for i in range (2, int(sqrt(n))+1):
         while n % i == 0:
             n //= i
         if n == 1:
             return i
     return n
+        
         
 assert largest_prime_factor(2) == 2
 assert largest_prime_factor(10) == 5
