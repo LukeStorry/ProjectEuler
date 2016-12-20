bef0fb29d1443565ff8cb30445a0f8979ab0ffd9
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 21:08:26 +0000

    Update 007.hs

--- 007.hs
+++ 007.hs
@@ -5,6 +5,15 @@
 
 -- Using Haskell to make the most of Lazy evaluation there.
 
+
+nth :: Int -> Int
+nth = (primes !!).pred
+
+primes :: [Int]
+primes = 2 : sieve [3,5..]
+    where
+        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0] 
+
 main :: IO ()
 main = do
     t <- readLn :: IO Int
