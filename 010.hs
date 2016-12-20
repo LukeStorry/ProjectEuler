254badd5f241444a4264121d3dacc6be588bd892
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 23:07:46 +0000

    solved 10

--- 010.hs
+++ 010.hs
@@ -1,6 +1,21 @@
 -- Solution to Project Euler Problem 10
--- https://projecteuler.net/problem=10
 
+-- https://projecteuler.net/problem=10
 -- Find the sum of all the primes below two million.
 
-reusing lazy prime evaluation from #7
+-- https://www.hackerrank.com/contests/projecteuler/challenges/euler010
+-- The first line contains t, the number of the test cases, the next t lines will contain an integer N.
+
+--reusing lazy prime evaluation from #7
+
+sumprimes :: Int -> Int
+sumprimes n = sum $ takeWhile (<=n) primes
+
+primes :: [Int]
+primes = 2 : sieve [3,5..]
+    where
+        sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0] 
+
+main :: IO ()
+main = print $ sumprimes 2000000
+--timeout?
