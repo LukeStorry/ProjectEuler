88ca7491f3d3340c1b2f407369375bd48c647e2d
Luke Storry <luke@lukestorry.co.uk>
2016-12-20 21:10:20 +0000

    Create 008.py

---
+++ 008.py
@@ -0,0 +1,6 @@
+
+# Solution to Project Euler Problem 8
+# https://projecteuler.net/problem=8
+
+# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+# HR extension: Find the greatest product of  consecutive digits in the  digit number.
