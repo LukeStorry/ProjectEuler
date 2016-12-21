a5e7d0c22120de58a9c4291e87d05728f0c89194
Luke Storry <luke@lukestorry.co.uk>
2016-12-21 00:17:13 +0000

    started 12

---
+++ 012.hs
@@ -0,0 +1,13 @@
+-- Solution to Project Euler Problem 12
+-- https://projecteuler.net/problem=12
+-- What is the value of the first triangle number to have over five hundred divisors?
+
+-- Uses haskell to use lazy evaluation of triangle generation
+
+euler12 :: Int -> Int
+euler12 n = head . filter ((>n) . numberOfDivisors) triangleNumbers
+
+numberOfDivisors :: Int -> Int
+numberOfDivisors n = length [ x | x <- [1..n], n `mod` x == 0]
+
+triangleNumbers = scanl1 (+) [1..]
