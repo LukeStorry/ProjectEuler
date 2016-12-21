89ad829090967230597f3ce1dd71464dae2a6a86
Luke Storry <luke@lukestorry.co.uk>
2016-12-21 19:25:20 +0000

    removed second cache list

--- 014.py
+++ 014.py
@@ -3,36 +3,38 @@
 
 # Longest Collatz sequence
 # Which starting number, under one million, produces the longest chain?
-
 import sys
 
-# varies by system - memory causes segfaults
-limit = 100000000
+#any higher and memory causes segfaults
+limit = 10000000
 
 # for memoization
-arr_next = [1, 1, 1] + [0]*limit
-arr_dist = [0, 0, 1] + [0]*limit
+arr_steps = [0, 0, 1] + [-1]*limit
         
-def calc_cell(n):
-    global arr_next, arr_dist
-    if n >= len(arr_next):
-        print("Error: n=" +str(n))
-        exit()
-    if arr_next[n] == 0:
+def calc_dist(n):
+    global arr_steps
+    
+    # ignore things above limit
+    if n > limit:
+        return 1
+    
+    # if no value yet, calculate it
+    if arr_steps[n] == -1:
         next_val = n//2 if n%2==0 else 3*n+1
-        arr_next[n] = next_val
-        arr_dist[n] = calc_cell(next_val)[1] + 1
-    return (arr_next[n], arr_dist[n])
+        arr_steps[n] = calc_dist(next_val) + 1
+        
+    return arr_steps[n]
 
 def euler14 (n):
     best_start = 0
     best_steps = 0
     for start in range(n+1):
-        (_, steps) = calc_cell(start)
+        steps = calc_dist(start)
         if steps >= best_steps:
             best_start = start
             best_steps = steps
     return best_start
+
 
 #Due to memory limits, max 25000
 print(euler14(25000))
